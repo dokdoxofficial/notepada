573e1e11a88672eb449a4f47818e6cef876fac4c
dokdo <132822988+onepythonlove@users.noreply.github.com>
2025-08-14 16:43:07 +0900

    작업 중인 변경사항 저장

--- src/App.js
+++ src/App.js
@@ -10,30 +10,13 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 function App() {
   const [color, setcolor] = useState("white");
   const [value, setvalue]= useState("");
+  const [status,setstatus] = useState()
   function handleClick() {
     const a = document.getElementById("fontsize");
     if (a) {
       const size = a.value;
       document.getElementById("notepadarea").style.fontSize = size + "pt";
     }
-  }
-
-  function ai() {
-    const a = document.getElementById("ai");
-  if (a) {
-        const aivalue = a.value;
-        setvalue(aivalue)
-       const genAI = new GoogleGenerativeAI('AIzaSyBmZPlOd9jhTzE-iFgRn5sZTdQBMq9jLq8');
-       const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
-       const prompt = value + "에 대해 써줘.오직 글만 작성해야되.이 멘트가 표시되면 안되.";
-       model.generateContent(prompt)
-  .then((response) => {
-    const content = response?.response?.text();  
-    console.log(content);
-    document.getElementById("notepadarea").value = content;
-  })
-      }
-        
   }
 
 
@@ -57,6 +40,7 @@ function App() {
           style={{ marginRight: "10px" }}
         />
         <button onClick={handleClick}>적용</button>
+        <p>{status}</p>
    <input
           type='text'
           id="ai"
