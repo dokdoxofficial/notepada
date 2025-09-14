7f4321d7bd50da2223f698dee9ecb6ca38630e00
dokdox <dokdoxisgreat@gmail.com>
2025-09-14 10:15:54 +0900

    메시지 변경으로 더 직관적인 메모서비스사용가능

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 function App() {
   //usestate
   const [color, setcolor] = useState("white");
-  const [value, setvalue]= useState("필기");
+  const [value, setvalue]= useState("메모의 중요성");
   const [loading, setloading] = useState("");
   //variable
   const [va, setva] = useState(undefined);
@@ -21,20 +21,20 @@ function App() {
   function ai() {
     const a = document.getElementById("ai")
   
-  if (a) {
+   if (a) {
        const aivalue = a.value;
        setvalue(aivalue)
-       setloading("로딩중:(")
+       setloading("작성하는중...(1/1)")
        const genAI = new GoogleGenerativeAI(process.env.REACT_APP_API_KEY);
        const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
-       const prompt = value + "에 대한 글을 199자 로 작성";
+       const prompt = value + "에 대한 글을 199자 로 작성해주십시오.";
        model.generateContent(prompt)
        
   .then((response) => {
     const originaldata=localStorage.getItem("notepadvalue");
     const content = response?.response?.text();  
-    document.getElementById("notepadarea").value= content +originaldata ;
-    setloading("AI작성을 완료함:)");
+    document.getElementById("notepadarea").value= content + originaldata ;
+    setloading("작성이 완료되었습니다.");
     document.getElementById("loading").style.display="block";
   })
   }
@@ -45,8 +45,7 @@ setInterval(() => {
   const notepad = document.getElementById("notepadarea");
   if (notepad) {
     localStorage.setItem("notepadvalue", notepad.value);
-     console.log("저장됨(5000ms)")
-    setva("작성한 내용을 저장함:)")
+    setva("저장되었습니다.")
   
   
   }
@@ -57,7 +56,7 @@ window.addEventListener("load", () => {
   const notepadvalue = localStorage.getItem("notepadvalue");
   if (notepadvalue !== null) {
     document.getElementById("notepadarea").value = notepadvalue;
-    setva("저장된 내용을 불러옴:)")
+    setva("저장된 내용을 불러왔습니다.")
   }
 });
 
@@ -78,19 +77,20 @@ window.addEventListener("load", () => {
           placeholder='글자크기를 임력:예시)10,20 등'
           style={{ marginRight: "10px" }}
         />
-        <button onClick={handleClick}>적용</button>
+        <button onClick={handleClick}>적용</button>        
+        <input type="color" onChange={e => setcolor(e.target.value)} />
         <input
           type='text'
           id="ai"
-          placeholder='AI에게 도움을 받아작성을 해보세요.예시)오늘을 위한 시를 적어줘'
+          placeholder='AI에게 도움을 받아작성을 해보세요.'
           style={{ marginRight: "10px" }}
         />
          <button onClick={ai}>작성요청</button>
-         <input type="color" onChange={e => setcolor(e.target.value)} />
+
          
          </div>
          <textarea id="notepadarea"style={{color:color}}></textarea>  
-         <h5>저작자표시</h5>
+         <h5>copyright</h5>
          <a href="https://www.flaticon.com/free-icons/document" title="document icons">Document icons created by Freepik - Flaticon</a>
  
     </div>
