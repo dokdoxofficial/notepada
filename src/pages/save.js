bc0718d579a694507b1992eac644cdd34b25a15d
dokdoxofficial <onepythonlove@gmail.com>
2025-10-06 23:30:37 +0900

    Update save.js

--- src/pages/save.js
+++ src/pages/save.js
@@ -12,16 +12,14 @@ export default function Save() {
   };
   return (
     <>
-     <Link to="/">
+     <a herf="https://notepadx.netlify.app">
          <img src={savelogo} id ="savelogo" alt='저장/공유 등을 할려면 이버튼을 클릭해주세요.' />
-     </Link>
+    </a>
      <h3>공유</h3>
      <p>현재 작성한 노트를 공유하십시오.</p>
      <button id="share" onClick={()=>{navigator.share(data)}}>공유</button>
      <hr></hr>
      <div id = "setting"></div>
-     <h3>새로운 메모를 추가(새창에서)</h3>
-     <a href='https://notepadx.netlify.app' target='_blank' without rel="noreferrer" >빠른 추가</a>
      <hr></hr>
      <div id="update">
       <h3>새로운 기능</h3>
